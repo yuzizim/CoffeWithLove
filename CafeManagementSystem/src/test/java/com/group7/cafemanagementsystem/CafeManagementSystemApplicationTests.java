c7799abb06e3245d89742fa9a624ea0281695601
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-08 22:54:23 +0700

    Init project

---
+++ CafeManagementSystem/src/test/java/com/group7/cafemanagementsystem/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.cafemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
