8d2340385d550627b72def5cc9c5ed86d8dfc879
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-17 01:00:10 +0700

    feat: add BillInfo model

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/BillInfo.java
@@ -0,0 +1,29 @@
+package com.group7.cafemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Bill")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BillInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "idBill", nullable = false)
+    private Bill bill;
+
+    @OneToOne
+    @JoinColumn(name = "idFood")
+    private Food food;
+
+    @Column(name = "count")
+    private int count;
+}
