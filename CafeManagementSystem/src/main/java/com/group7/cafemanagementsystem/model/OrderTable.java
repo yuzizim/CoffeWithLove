6f619e0d1e1526ef19dad2e0a05e04b8646777ce
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-17 01:00:32 +0700

    feat: add OrderTable model

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/OrderTable.java
@@ -0,0 +1,42 @@
+package com.group7.cafemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "OrderTable")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "OrderDate")
+    private Date orderDate;
+
+    @Column(name = "OrderTime")
+    private LocalTime orderTime;
+
+    @Column(name = "CustomerName")
+    private String customerName;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "PhoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "NumberOfPeole")
+    private int numPeople;
+
+    @Column(name = "Note")
+    private String note;
+}
