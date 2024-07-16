0110ada4ca38a3e2e8c9ff1fbf1520b14ffcf24f
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-17 01:00:24 +0700

    feat: add FoodCategory model

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/FoodCategory.java
@@ -0,0 +1,28 @@
+package com.group7.cafemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "FoodCategory")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "foodCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Food> foods = new ArrayList<>();
+}
