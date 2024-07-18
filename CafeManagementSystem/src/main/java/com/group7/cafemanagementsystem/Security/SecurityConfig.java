65f9218343096cc7ff00f6c1436b4d43c3680d7f
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-18 10:56:06 +0700

    fix: remove redundant /

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/SecurityConfig.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/**/").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admin/**").hasAnyAuthority("ADMIN")
                         .requestMatchers("/api/staff/**").hasAnyAuthority("STAFF")
                         .anyRequest().authenticated())
