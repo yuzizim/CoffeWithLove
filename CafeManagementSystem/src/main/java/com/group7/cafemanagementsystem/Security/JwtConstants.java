565929b9822f57cbc34b38eab73efd6a6aae7fa7
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-18 10:56:22 +0700

    fix: change key

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
@@ -1,7 +1,7 @@
 package com.group7.cafemanagementsystem.Security;
 
 public class JwtConstants {
-    public static final String SECRET_KEY = "XjRSbTAHGydaJmYHsDBfmQ==";
+    public static final String SECRET_KEY = "zzKuADl6R1vgbGYESb7meWuGvONSRgJ0Ps3rJZkP1HiYCpnsqu3BcZY2usMPxI8A";
     public static final long EXPIRE_DATE = 60 * 60 * 1000;
     public static final String BEARER = "BEARER ";
 }
