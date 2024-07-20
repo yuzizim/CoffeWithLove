c08a792d4768c21af5b8caa1c04f23feb5403a29
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-20 21:38:03 +0700

    feat: add refreshtoken name and expire date

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
@@ -4,4 +4,7 @@ public class JwtConstants {
     public static final String SECRET_KEY = "zzKuADl6R1vgbGYESb7meWuGvONSRgJ0Ps3rJZkP1HiYCpnsqu3BcZY2usMPxI8A";
     public static final long EXPIRE_DATE = 60 * 60 * 1000;
     public static final String BEARER = "BEARER ";
+    public static final String JWT_COOKIE_NAME = "token";
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+    public static final long REFRESH_EXPIRE_DATE = 30 * 24 * 60 * 60 * 1000;
 }
