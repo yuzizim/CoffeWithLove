b564ab2fed5b806e8b97783fc609735987d83f2d
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-21 21:56:25 +0700

    feat: add method homecontroller to redirect home

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Home/HomeController.java
@@ -0,0 +1,24 @@
+package com.group7.cafemanagementsystem.Controller.Home;
+
+import com.group7.cafemanagementsystem.Repository.UserRepository;
+import com.group7.cafemanagementsystem.model.Account;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+@AllArgsConstructor
+public class HomeController {
+    UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home() {
+        return "/dist/index";
+    }
+}
