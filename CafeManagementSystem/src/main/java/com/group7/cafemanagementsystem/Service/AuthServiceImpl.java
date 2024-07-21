4a48d0adcf3cc7d8f25cf0d092f64008c7f91bd8
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-21 21:57:06 +0700

    fix: change method login and add method to refreshtoken

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Service/AuthServiceImpl.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Service/AuthServiceImpl.java
@@ -2,17 +2,26 @@ package com.group7.cafemanagementsystem.Service;
 
 import com.group7.cafemanagementsystem.Repository.UserRepository;
 import com.group7.cafemanagementsystem.Request.LoginRequest;
+import com.group7.cafemanagementsystem.Request.RefreshRequest;
+import com.group7.cafemanagementsystem.Response.AuthenticationResponse;
 import com.group7.cafemanagementsystem.Response.LoginResponse;
 import com.group7.cafemanagementsystem.Security.JwtProvider;
 import com.group7.cafemanagementsystem.model.Account;
+import com.group7.cafemanagementsystem.model.RefreshToken;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
 @AllArgsConstructor
@@ -21,24 +30,56 @@ public class AuthServiceImpl implements AuthService {
     private AuthenticationManager authenticationManager;
     private JwtProvider jwtProvider;
     private PasswordEncoder passwordEncoder;
+    private RefreshTokenService refreshTokenService;
 
     @Override
-    public String login(LoginRequest request) {
-//        Account user = userRepository.findByUserName(request.getUserName()).get();
-//        String password = request.getPassword();
-//        if(!passwordEncoder.matches(password, user.getPassword())){
-//            throw new BadCredentialsException("You provided an incorrect password!");
-//        }
+    public String login(Model model, @ModelAttribute LoginRequest request, HttpServletResponse response) {
         String name = request.getUserName();
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUserName(),
-                        request.getPassword()
-                )
-        );
+        String token = null;
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUserName(),
+                            request.getPassword()
+                    )
+            );
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtProvider.generateToken(authentication);
-        return token;
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            token = jwtProvider.generateToken(authentication);
+
+            Account account = new Account();
+            account.setUserName(request.getUserName());
+            ResponseCookie jwtCookie = jwtProvider.generateJwtCookie(account);
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authentication);
+            ResponseCookie jwtResponseCookie = jwtProvider.generateRefreshJwtCookie(refreshToken.getToken());
+
+            // Add cookies to response
+            jwtProvider.addCookieToResponse(response, jwtCookie);
+            jwtProvider.addCookieToResponse(response, jwtResponseCookie);
+        } catch (UsernameNotFoundException e) {
+            return "Bad credentials";
+        } catch (BadCredentialsException ex) {
+            return "Bad credentials";
+        }
+
+//        if (token != null) {
+//            Cookie cookie = new Cookie("token", token);
+//            cookie.setHttpOnly(true); // Prevents JavaScript access to the cookie
+//            cookie.setSecure(true); // Ensure the cookie is sent over HTTPS only (set this to false if you're testing locally over HTTP)
+//            cookie.setPath("/");
+//            cookie.setMaxAge(Integer.MAX_VALUE);
+//            response.addCookie(cookie);
+//        } else {
+//            return "Token is not valid";
+//        }
+        return "Login success";
+    }
+
+    @Override
+    public AuthenticationResponse refreshToken(RefreshRequest requests) {
+//        var signedJwt = jwtProvider.vailidateToken(requests.getToken());
+//
+//        var jid = signedJwt.get
+        return null;
     }
 }
