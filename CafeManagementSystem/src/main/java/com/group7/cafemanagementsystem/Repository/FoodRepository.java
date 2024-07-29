cb7b2895fbeb83d70671978bd9d8a2aae99d88d3
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-29 10:51:42 +0700

    refactor: add method to get revenue by day

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Repository/FoodRepository.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Repository/FoodRepository.java
@@ -1,12 +1,16 @@
 package com.group7.cafemanagementsystem.Repository;
 
+import com.group7.cafemanagementsystem.Response.FoodRevenueResponse;
 import com.group7.cafemanagementsystem.model.Food;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +21,18 @@ public interface FoodRepository extends JpaRepository<Food, Integer> {
     Page<Food> findByStatusOrderByStatus(Pageable pageable);
 
     Optional<Food> findById(int id);
+
+    @Query("SELECT f.name AS name, (f.price * sum(bi.count)) AS price, sum(bi.count) as numSale " +
+            " FROM BillInfo bi " +
+            " JOIN Bill b on bi.bill.id = b.id " +
+            " JOIN Food f on bi.food.id = f.id " +
+            " WHERE b.dateCheckIn = :day" +
+            " GROUP BY f.name, f.price")
+    List<FoodRevenueResponse> getFoodRevenueByDay(@Param("day") LocalDateTime day);
+
+    @Query(value = "SELECT SUM(bi.count) AS price\n" +
+            "    FROM bill_info as bi\n" +
+            "    JOIN Bill b ON bi.id_bill = b.id\n" +
+            "    JOIN Food f ON bi.id_food = f.id", nativeQuery = true)
+    Object totalProductSold();
 }
