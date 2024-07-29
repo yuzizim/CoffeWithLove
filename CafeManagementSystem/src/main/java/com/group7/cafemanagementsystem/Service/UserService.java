2208850b3d225aa6e978f9c02dffee79f51ad0c0
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-29 10:49:55 +0700

    feat: add user service

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Service/UserService.java
@@ -0,0 +1,18 @@
+package com.group7.cafemanagementsystem.Service;
+
+import com.group7.cafemanagementsystem.Response.PageUserResponse;
+import com.group7.cafemanagementsystem.model.Account;
+
+public interface UserService {
+    PageUserResponse getUserByPage(Boolean status, String search, int page, int size);
+
+    Account saveAccount(Account account);
+
+    Account deleteAccount(int id);
+
+    Account changeStatusAccount(int id);
+
+    Account findById(int id);
+
+    boolean findByUserName(String username);
+}
---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Service/UserServiceImpl.java
@@ -0,0 +1,77 @@
+package com.group7.cafemanagementsystem.Service;
+
+import com.group7.cafemanagementsystem.Repository.UserRepository;
+import com.group7.cafemanagementsystem.Response.PageFoodResponse;
+import com.group7.cafemanagementsystem.Response.PageUserResponse;
+import com.group7.cafemanagementsystem.model.Account;
+import com.group7.cafemanagementsystem.model.Food;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public PageUserResponse getUserByPage(Boolean status, String search, int page, int size) {
+        if (search == null) search = "";
+        List<Account> accounts = new ArrayList<>();
+        Pageable paging = PageRequest.of(page, size);
+
+        Page<Account> pageAccounts;
+        if (status == null) {
+            pageAccounts = userRepository.findByStatusOrderByStatus(paging, search);
+        } else {
+            pageAccounts = userRepository.findByStatus(status, paging);
+        }
+
+        accounts = pageAccounts.getContent();
+        int totalPages = pageAccounts.getTotalPages();
+        return new PageUserResponse(accounts, totalPages);
+    }
+
+    @Override
+    public Account saveAccount(Account account) {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        account.setRole("STAFF");
+        account.setStatus(true);
+        account.setAvatar("/static/img/account/user.png");
+        return userRepository.save(account);
+    }
+
+    @Override
+    public Account deleteAccount(int id) {
+        Account account = userRepository.findById(id).get();
+        account.setStatus(false);
+        return userRepository.save(account);
+    }
+
+    @Override
+    public Account changeStatusAccount(int id) {
+        Account account = userRepository.findById(id).get();
+        account.setStatus(!account.isStatus());
+        return userRepository.save(account);
+    }
+
+    @Override
+    public Account findById(int id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public boolean findByUserName(String username) {
+        return userRepository.findByUserName(username).isPresent();
+    }
+}
