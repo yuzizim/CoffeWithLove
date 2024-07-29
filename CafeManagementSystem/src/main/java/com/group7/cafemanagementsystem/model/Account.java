e7d2e8b76622fcb95463ef68936880ce64745cbd
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-29 10:49:40 +0700

    refactor: add annotation check valid

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/Account.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/Account.java
@@ -1,6 +1,10 @@
 package com.group7.cafemanagementsystem.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,16 +21,34 @@ public class Account {
     private int ID;
 
     @Column(name = "UserName")
+    @NotEmpty(message = "Username is required")
+    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Username can only contain letters, numbers, and underscores")
     private String userName;
 
     @Column(name = "DisplayName")
+    @NotEmpty(message = "Full name is required")
     private String fullName;
 
     @Column(name = "Email")
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email is required")
     private String email;
 
     @Column(name = "Password")
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, message = "Password should have at least 6 characters")
     private String password;
+
+    @Column(name = "phone")
+    @NotEmpty(message = "Phone number is required")
+    @Pattern(regexp = "^\\d{10}$", message = "Phone number should be 10 digits")
+    private String phoneNumber;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "status")
+    private boolean status;
 
     @Column(name = "role")
     private String role;
