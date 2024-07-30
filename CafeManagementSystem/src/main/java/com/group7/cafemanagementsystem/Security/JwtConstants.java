c877fd56bb224ffa3135053e898944b900e97636
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-30 14:54:17 +0700

    refactor: change expire date

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Security/JwtConstants.java
@@ -2,7 +2,7 @@ package com.group7.cafemanagementsystem.Security;
 
 public class JwtConstants {
     public static final String SECRET_KEY = "zzKuADl6R1vgbGYESb7meWuGvONSRgJ0Ps3rJZkP1HiYCpnsqu3BcZY2usMPxI8A";
-    public static final long EXPIRE_DATE = 60 * 60 * 1000;
+    public static final long EXPIRE_DATE = 24 * 60 * 60 * 1000;
     public static final String BEARER = "BEARER ";
     public static final String JWT_COOKIE_NAME = "token";
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
