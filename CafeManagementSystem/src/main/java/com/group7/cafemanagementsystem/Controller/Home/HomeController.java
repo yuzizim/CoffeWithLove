f57efd8e498cd9500cc2a9b3ffe7d517a10707ca
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-30 14:54:32 +0700

    refactor: change home page

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Home/HomeController.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Home/HomeController.java
@@ -1,26 +1,48 @@
 package com.group7.cafemanagementsystem.Controller.Home;
 
 import com.group7.cafemanagementsystem.Repository.UserRepository;
+import com.group7.cafemanagementsystem.Service.CartService;
+import com.group7.cafemanagementsystem.Service.FoodCategoryService;
 import com.group7.cafemanagementsystem.model.Account;
+import com.group7.cafemanagementsystem.model.Cart;
+import com.group7.cafemanagementsystem.model.FoodCategory;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/staff/home")
 public class HomeController {
     UserRepository userRepository;
+    CartService cartService;
 
     @GetMapping
-    public String home() {
-        return "/dist/index";
+    public String home(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        List<Cart> carts = cartService.getCartByUser(username);
+        model.addAttribute("numInCart", carts.size());
+        model.addAttribute("username", username);
+
+        return "/staff/index";
     }
 }
