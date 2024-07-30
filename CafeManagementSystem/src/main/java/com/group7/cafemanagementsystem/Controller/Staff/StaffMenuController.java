e9141b9cf86e9fb6df0d4c20f44e519889054eb8
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-30 14:53:52 +0700

    feat: add Staff controller

---
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Staff/StaffMenuController.java
@@ -0,0 +1,80 @@
+package com.group7.cafemanagementsystem.Controller.Staff;
+
+import com.group7.cafemanagementsystem.Response.PageFoodResponse;
+import com.group7.cafemanagementsystem.Service.CartService;
+import com.group7.cafemanagementsystem.Service.FoodCategoryService;
+import com.group7.cafemanagementsystem.Service.FoodService;
+import com.group7.cafemanagementsystem.model.Cart;
+import com.group7.cafemanagementsystem.model.Food;
+import com.group7.cafemanagementsystem.model.FoodCategory;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/staff/manage")
+public class StaffMenuController {
+    private FoodService foodService;
+    private CartService cartService;
+    private FoodCategoryService foodCategoryService;
+
+    @GetMapping("/menu")
+    public String displayMenu(Model model,
+                              @RequestParam(name = "category", defaultValue = "-2") int id,
+                              @RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "8") int size) {
+        if (id == -2) {
+            PageFoodResponse response = foodService.getMenuByPage(page, size);
+            model.addAttribute("menu", response.getFoods());
+            model.addAttribute("pageSize", response.getTotalPages());
+        } else {
+            if (id == -1) {
+                return "redirect:/staff/manage/menu";
+            } else {
+                PageFoodResponse response = foodService.getFoodByCategoryId(id, page, size);
+                model.addAttribute("menu", response.getFoods());
+                model.addAttribute("pageSize", response.getTotalPages());
+            }
+        }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        List<FoodCategory> categories = foodCategoryService.getFoodCategories();
+        List<Cart> carts = cartService.getCartByUser(username);
+        model.addAttribute("categories", categories);
+        model.addAttribute("pageNumber", page);
+        model.addAttribute("cateId", id);
+        model.addAttribute("numInCart", carts.size());
+        return "/staff/shop";
+    }
+
+    @PostMapping("/add-to-cart/{id}")
+    public String addItemToCart(@PathVariable int id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        cartService.addItemToCart(id, username);
+        return "redirect:/staff/manage/menu";
+    }
+}
