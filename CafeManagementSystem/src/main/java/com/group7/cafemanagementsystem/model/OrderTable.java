3abcbe2716dfe1d3993896f58a33beed6aeac715
Dao Tan Phat <daotanphat2143@gmail.com>
2024-08-02 22:29:09 +0700

    refactor: remove some field

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/OrderTable.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/model/OrderTable.java
@@ -1,6 +1,7 @@
 package com.group7.cafemanagementsystem.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "OrderTable")
@@ -26,20 +28,30 @@ public class OrderTable {
     @Column(name = "CustomerName")
     private String customerName;
 
-    @Column(name = "Email")
-    private String email;
-
     @Column(name = "PhoneNumber")
     private String phoneNumber;
 
+    @Min(value = 1, message = "Number of people cannot be smaller than 1")
     @Column(name = "NumberOfPeole")
     private int numPeople;
 
     @Column(name = "Note")
     private String note;
 
+    @Column(name = "TotalPrice")
+    private double totalPrice;
+
+    @Column(name = "Status")
+    private boolean status;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TableFood", nullable = false)
     private TableFood tableFood;
 
+    @OneToMany(mappedBy = "orderTable", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderDetail> orderDetails;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Account staff;
 }
