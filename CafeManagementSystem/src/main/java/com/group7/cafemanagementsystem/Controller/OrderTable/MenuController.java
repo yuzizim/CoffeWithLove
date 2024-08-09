b52cde413e38c32db70efc7a68fed0cfefab592b
Le Quang Khai (K17 HL) <khailqhe170154@fpt.edu.vn>
2024-08-09 07:02:31 +0700

    feat: add menu public feature

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/OrderTable/MenuController.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/OrderTable/MenuController.java
@@ -1,10 +1,15 @@
 package com.group7.cafemanagementsystem.Controller.OrderTable;
 
+import com.group7.cafemanagementsystem.Response.PageFoodResponse;
 import com.group7.cafemanagementsystem.Service.*;
 import com.group7.cafemanagementsystem.model.Food;
+import com.group7.cafemanagementsystem.model.FoodCategory;
 import com.group7.cafemanagementsystem.model.TableFood;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +20,50 @@ import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/menu")
+@RequestMapping("/ECoffee/menu")
 public class MenuController {
     private final FoodService foodService;
     private CartService cartService;
     private FoodCategoryService foodCategoryService;
 
-    @GetMapping("/view")
+    @GetMapping()
     public String viewTables(Model model,@RequestParam(name = "category", defaultValue = "-2") int id,
                              @RequestParam(name = "search", defaultValue = "") String search,
                              @RequestParam(name = "orderId", defaultValue = "-1") int orderId,
                              @RequestParam(defaultValue = "0") int page,
                              @RequestParam(defaultValue = "9") int size) {
+        search = search.trim();
+        if (id == -2) {
+            PageFoodResponse response = foodService.getMenuByPageAndSearch(search, page, size);
+            model.addAttribute("menu", response.getFoods());
+            model.addAttribute("pageSize", response.getTotalPages());
+        } else {
+            if (id == -1) {
+                if (orderId == -1) {
+                    return "redirect:/ECoffee/menu";
+                } else {
+                    return "redirect:/ECoffee/menu?orderId=" + orderId;
+                }
+            } else {
+                PageFoodResponse response = foodService.getFoodByCategoryIdAndSearchKey(id, search, page, size);
+                model.addAttribute("menu", response.getFoods());
+                model.addAttribute("pageSize", response.getTotalPages());
+            }
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        List<FoodCategory> categories = foodCategoryService.getFoodCategories();
+        model.addAttribute("categories", categories);
+        model.addAttribute("pageNumber", page);
+        model.addAttribute("cateId", id);
+        model.addAttribute("search", search);
+        model.addAttribute("username", username);
         return "/dist/menu";
     }
 }
