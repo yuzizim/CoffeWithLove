59ddc976db676601a58a5e535aa6d37652c2312b
Dao Tan Phat <daotanphat2143@gmail.com>
2024-08-11 23:07:13 +0700

    refactor: add display list empty table

--- CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Home/HomeController.java
+++ CafeManagementSystem/src/main/java/com/group7/cafemanagementsystem/Controller/Home/HomeController.java
@@ -3,9 +3,11 @@ package com.group7.cafemanagementsystem.Controller.Home;
 import com.group7.cafemanagementsystem.Repository.UserRepository;
 import com.group7.cafemanagementsystem.Service.CartService;
 import com.group7.cafemanagementsystem.Service.FoodCategoryService;
+import com.group7.cafemanagementsystem.Service.TableFoodService;
 import com.group7.cafemanagementsystem.model.Account;
 import com.group7.cafemanagementsystem.model.Cart;
 import com.group7.cafemanagementsystem.model.FoodCategory;
+import com.group7.cafemanagementsystem.model.TableFood;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -26,6 +28,7 @@ import java.util.List;
 public class HomeController {
     UserRepository userRepository;
     CartService cartService;
+    TableFoodService tableFoodService;
 
     @GetMapping
     public String home(Model model) {
@@ -40,8 +43,10 @@ public class HomeController {
         }
 
         List<Cart> carts = cartService.getCartByUser(username);
+        List<TableFood> tables = tableFoodService.getAllTablesEmpty();
         model.addAttribute("numInCart", carts.size());
         model.addAttribute("username", username);
+        model.addAttribute("tables", tables);
 
         return "/staff/index";
     }
